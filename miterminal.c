db5359f065c25e1cef3b9799f605e6ab9158bd43
Gquilodran <gquilodran2024@udec.cl>
2025-09-24 18:01:02 -0300

    bases de la shell

---
+++ miterminal.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+#include <setjmp.h>
+#include <signal.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int exitCmd(char text[]){
+	text[strcspn(text, "\n")] = '\0';
+	if(strcmp(text,"exit")==0){
+		return 1; //el usuario quiere salir
+	}else{
+		return 0;
+	}
+}
+
+
+char** separaCmd(char text[], char *argv[]){
+	int j=0;
+	int inWord=0; //variable auxiliar para contar si estamos o no dentro de una palabra
+	for(int i=0; text[i]!='\0'; i++){
+		if(text[i]!=' ' && text[i] != '\n'){
+			if(!inWord){
+				argv[j++]=&text[i];
+				inWord=1;
+			}
+		}else{ //en el caso que detecte un espacio, considera que ya acabo la palabra
+			text[i]='\0';
+			inWord=0;
+		}
+	}
+	argv[j]=NULL;
+	return argv;
+}
+void creaProceso(char text[]){
+	char *argv[50];
+	separaCmd(text, argv);
+	pid_t pid = fork();
+	if(pid==0){
+		execvp(argv[0], argv);
+		printf("Se creo un proceso hijo\n"); //referencial mas que nada: borrar
+		perror("Error, comando ingresado no detectable\n");
+		exit(1);
+	}else{
+		wait(NULL);
+		printf("Proceso padre\n"); //linea referenciar: borrar
+	}
+}
+
+int main(){
+	printf("=======================================================================================================\n");
+	printf("Bienvenido a la Mi Shell, tarea 1 asignatura Sistemas Operativos, impartida el segundo semestre 2025.\n");
+	printf("La shell presentada fue desarrollada por los alumno: ... \n");
+	printf("Ingrese el comando deseado o en caso de que desee salir, precione Ctrl+C\n");
+	printf("=======================================================================================================\n");
+	printf("\n");
+
+	while(1){ //se repetira infinitamente hasta que el usuario escriba "exit" o precione Ctrl+c
+		printf("mishell:$ ");
+		char text_usr[50];
+		fgets(text_usr, sizeof(text_usr), stdin);
+		if(text_usr[0] =='\n'){
+			continue;
+		}else{
+			if(exitCmd(text_usr)==1){
+				printf("===== Hasta luego usuario =====\n");
+			    return 0;
+			}
+			creaProceso(text_usr);
+		}
+	}
+	return 0;
+}
